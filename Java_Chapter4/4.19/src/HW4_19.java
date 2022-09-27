fb4815687f6ebc4a84cd8785b233559b85dd49a9
Star <109724704+Star369666@users.noreply.github.com>
2022-09-27 14:18:41 +0800

    Add files via upload

--- Java_Chapter4/4.19/src/HW4_19.java
+++ Java_Chapter4/4.19/src/HW4_19.java
@@ -1,20 +1,29 @@
-import java.util.Random;
+import java.util.*;
 
 public class HW4_19 {
 	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
 		double[] value = {239.99, 129.75, 99.95, 350.89};
-		Random random = new Random();
-		int[] item_sold = new int[4];
-		int i;
+		int i, item_number = 4;
+		int[] item_sold = new int[item_number];
 		double calculator, total = 200.0;
 		
-		for(i = 0; i < value.length; i++) {
-			item_sold[i] = Math.abs(random.nextInt());
-			calculator = item_sold[i]*value[i];
-			System.out.print("Item1: " + item_sold[i] + " ,which has " + calculator + " unit value. Receiving " + 0.09*calculator + " unit value.\n");
-			total += calculator*0.09;
+		try {
+			for(i = 0; i < item_number; i++) {
+				System.out.printf("Enter the %dth items's quantity: ", (i+1));
+				item_sold[i] = input.nextInt();
+				while(item_sold[i] < 0) {
+					System.out.printf("The quantity can't lower than zero, enter it again: ");
+					item_sold[i] = input.nextInt();
+				}
+				calculator = item_sold[i]*value[i];
+				System.out.printf("Item%d: %d are sold, which has %.2f unit value. Receiving %.2f unit value.\n", (i+1), item_sold[i], calculator, 0.09*calculator);
+				total += calculator*0.09;
+			}
+			System.out.printf("The total value given to the salesperson for last week earning is %.2f.\n", total);
 		}
-		System.out.print("The total value given to the salesperson for last week earning is " + total + ".\n");
-		
+		finally {
+			input.close();
+		}
 	}
 }
